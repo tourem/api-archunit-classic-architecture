2296cd262828a0ad3ab9e21e6e421a6304765c69
Mahamadou TOURE <touremohameda@gmail.com>
2021-06-16 00:44:15 +0200

    Create RestrictNumberOfClassesImplementsTest.java

---
+++ src/test/java/com/larbotech/archunit/classic/architecture/RestrictNumberOfClassesImplementsTest.java
@@ -0,0 +1,22 @@
+package com.larbotech.archunit.classic.architecture;
+
+
+import com.bnpp.zephyr.example.archunit.service.ClientService;
+import com.bnpp.zephyr.example.archunit.service.impl.ClientServiceImpl;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.base.DescribedPredicate.lessThanOrEqualTo;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = ArchitectureConstants.DEFAULT_PACKAGE)
+public class RestrictNumberOfClassesImplementsTest {
+
+    @ArchTest
+    static final ArchRule no_new_classes_should_implement_SendMoneyUseCase =
+            classes().that().implement(ClientService.class)
+                    .should().containNumberOfElements(lessThanOrEqualTo(1))
+                    .because("from now on new classes should implement " + ClientServiceImpl.class.getName());
+
+}
